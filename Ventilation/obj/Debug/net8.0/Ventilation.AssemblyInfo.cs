fd027759070ff3854863049ffed2cdda7f18073e
dinnerbone <ameenalmoosawi1234@gmail.com>
2025-08-12 17:50:18 -0400

    working on Player class

--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4348c83482955b016b030e9bf0ae9e907e77f3ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920f8f243e80e6c44dec6aeed88ba0a00e2c9e8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
