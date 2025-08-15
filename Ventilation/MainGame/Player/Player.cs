ddf4b7c0f6410e1fbed654093e6ad4accdb2a846
dinnerbone <ameenalmoosawi1234@gmail.com>
2025-08-15 17:11:24 -0400

    debugging Player Motion

--- Ventilation/MainGame/Player/Player.cs
+++ Ventilation/MainGame/Player/Player.cs
@@ -8,25 +8,29 @@ using GameComponents.Logic;
 namespace Main;
 public class Player : Entity 
 {
-    private PlayerMotion playerMovement;
+    private PlayerMotion playerMovement = new();
     // private fields
+    public Sprite PlayerSprite { get; private set; }
     public bool IsAlive { get; set; } = true;
     public bool IsControllable { get; set; } = true;
+    public float SpeedMultiplier { get; set; } = 1;
     public Player(int x, int y, int width, int height, float HP) : base(x, y, width, height, HP) 
-    {
-        playerMovement = new();
-    }
-    public void LoadContent(GraphicsDevice device) 
     {
         
     }
+    public void LoadContent(GraphicsDevice device) 
+    {
+        PlayerSprite = new(new Texture2D(device, 1, 1), Color.Red);
+        PlayerSprite.SetToData();
+    }
     public void UpdateLogic(GameTime gt) 
     {
+        MoveAndSlide(gt);
         playerMovement.HandleMotionStates(this);
     }
     public void Draw(SpriteBatch batch) 
     {
-        
+        PlayerSprite.Draw(batch, Bounds);
     }
     
 }
--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631d679736745ceb066f179c9fa5a5982010045a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a7dbcf6be158ede21529ad0880a0942c2e9e3de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
