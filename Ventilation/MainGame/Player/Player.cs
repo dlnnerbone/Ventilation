3c2ef31d0c0c46233cfc73538428139a43a5881e
dinnerbone <ameenalmoosawi1234@gmail.com>
2025-08-15 22:12:55 -0400

    adding more organization for player class and EVEN more remaking and tweaking.

--- Ventilation/MainGame/Player/Player.cs
+++ Ventilation/MainGame/Player/Player.cs
@@ -5,6 +5,7 @@ using GameComponents;
 using GameComponents.Entity;
 using GameComponents.Rendering;
 using GameComponents.Managers;
+using GameComponents.Logic;
 namespace Main;
 public class Player : Entity 
 {
@@ -14,13 +15,14 @@ public class Player : Entity
     private float maxSpeed = 750f;
     private float lerpSpeed = 0.15f;
     private float dashForce = 2000f;
+    private Timer dashCool, dashDur, staminaRegen;
     private int stamina = 3;
-    // private fields
+    // private fields for movement utilities
     public Motions SetMotion(Motions motion) => Motion = motion;
     public float MoveSpeed { get { return moveSpeed; } set { moveSpeed = value; } }
     public float MaxSpeed { get { return maxSpeed; } set { maxSpeed = value < 0 ? 0 : value; } }
     public float LerpSpeed { get { return lerpSpeed; } set { lerpSpeed = MathHelper.Clamp(value, 0f, 1f); } }
     public float DashForce { get { return dashForce; } set { dashForce = value <= maxSpeed ? value * 2 : value; } }
     public bool IsDashing { get; set; } = false;
-    
+    // player related variables
 }
