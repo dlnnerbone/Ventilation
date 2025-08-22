337488ef8afee0207b226a81f662d684bb544a17
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-08-22 12:36:09 -0400

    learning Flag Attributes and bit wise stuff with struct (Tile)

--- Ventilation/GameComponents/Structs/Tile.cs
+++ Ventilation/GameComponents/Structs/Tile.cs
@@ -1,15 +1,29 @@
 using Microsoft.Xna.Framework;
+using System;
+using GameComponents.Entity;
 namespace GameComponents;
-
+[Flags] public enum TileType 
+{
+    None = 0,
+    Collidable = 1,
+    Walkable = 2,
+    Dangerous = 4,
+    Movable = 8
+}
 public struct Tile 
 {
     private Rectangle sourceRegion;
+    private int tileID;
     // private stuff;
     public Rectangle SourceRegion { get { return sourceRegion; } private set { sourceRegion = value; } }
-    public int TileID;
+    public Vector2 TopLeft => new Vector2(Left, Top);
+    public Vector2 TopRight => new Vector2(Right, Top);
+    public Vector2 BottomLeft => new Vector2(Left, Bottom);
+    public Vector2 BottomRight => new Vector2(Right, Bottom);
+    public int TileID { get { return tileID; } set { tileID = value < 0 ? 0 : value; } }
     public int X { get { return sourceRegion.X; } set { sourceRegion.X = value; } }
     public int Y { get { return sourceRegion.Y; } set { sourceRegion.Y = value; } }
-    public Vector2 Position 
+    public Vector2 Location 
     {
         get => new Vector2(X, Y);
         set => sourceRegion.Location = new((int)value.X, (int)value.Y);
@@ -20,5 +34,23 @@ public struct Tile
     {
         get => new Vector2(Width, Height);
         set => sourceRegion.Size = new((int)value.X, (int)value.Y);
+    }
+    public float Left => sourceRegion.Left;
+    public float Right => sourceRegion.Right;
+    public float Bottom => sourceRegion.Bottom;
+    public float Top => sourceRegion.Top;
+
+    // bools
+    public TileType TileTypes { get; set; }
+    public bool IsCollidable => (TileTypes & TileType.Collidable) == TileType.Collidable;
+    public bool IsMovable => (TileTypes & TileType.Movable) == TileType.Movable;
+    public bool IsDangerous => (TileTypes & TileType.Dangerous) == TileType.Dangerous;
+    public bool IsWalkable => (TileTypes & TileType.Walkable) == TileType.Walkable;
+    public bool IntersectsWithTile(Rectangle other) => sourceRegion.Intersects(other);
+    public bool IntersectsWithTile(BodyComponent other) => sourceRegion.Intersects(other.Bounds);
+    public Tile(int x, int y, int width, int height, int ID) 
+    {
+        sourceRegion = new(x, y, width, height);
+        TileID = ID;
     }
 }
--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5318080eb789d778cec9b410a2301579d1e9a25a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8775b3ea7a1e0ba187a94f03611f551617f9ce0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
