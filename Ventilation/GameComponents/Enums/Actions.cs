9bb4fb49262f3b1d8d1acaaf1435723a098509ea
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-09-13 13:18:25 -0400

    adding more Ation states for futureproof.

--- Ventilation/GameComponents/Enums/Actions.cs
+++ Ventilation/GameComponents/Enums/Actions.cs
@@ -1,7 +1,13 @@
 namespace GameComponents;
 public enum Actions 
 {
-    Ready, // No Actions or in a static state.
-    Fly, // an action is initialted, (e.g: dashing, shooting, Projectile motion, etc)
-    End // an action has ended, or end an action.
+    Ready, // Action is waiting...
+    Charging, // Action is winding up or winding down.
+    Active, // Action is fully active
+    Interrupted, // action is interrupted.
+    Disabled, // action is disabled.
+    Transitioning, // action is transaitioning...
+    Ended, // action ended.
+    Completed, // action has successfully completed.
+    Cooldown // action is in recovery duration.
 }
--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c142850ecb289db53391c7fea9241cfe4a3feb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30266e260795e339638af5975bf9c1f5ea493ab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
