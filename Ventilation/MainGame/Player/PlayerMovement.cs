7c209a919673d8ebfe90099a61ec4a8ce1e8493a
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-09-25 13:55:37 -0400

    ading basic variables for player movement class

--- Ventilation/MainGame/Player/PlayerMovement.cs
+++ Ventilation/MainGame/Player/PlayerMovement.cs
@@ -1,9 +1,17 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using GameComponents;
+using GameComponents.Logic;
 using GameComponents.Managers;
 namespace Main;
 public class PlayerMovement 
 {
-    public interface IPlayer
+    private Timer dashCool, dashDur, staminaCool;
+    private InputManager input = new();
+    private float moveSpeed = 50f;
+    private float maxSpeed = 750f;
+    private float dashForce = 2000f;
+    private float speedMultiplier = 1f;
+    private float stamina = 100f;
+    private float maxStamina = 100f;
 }
