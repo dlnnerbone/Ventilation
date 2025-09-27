3cd5d5eb153bd083f5116eff5a595d3a57c9688d
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-09-27 12:10:49 -0400

    updating

--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c9f989cc7a5010d9bf5f5ded7bf9fca80583f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2df9bbabe7043071f5fece4c54fa975a963339")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
