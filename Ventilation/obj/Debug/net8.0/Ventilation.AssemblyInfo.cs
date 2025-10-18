eb5a7ff833ca8947808442cb14bf2db5670cb3cc
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-10-17 22:55:14 -0400

    adding a delegate method inside TextureDependecy

--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c090874284896e11ca9e763bbd988211f2b98256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b13727a7e70baada3bdcb3d23b864dfade846a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
