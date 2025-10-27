16296289f4690d73f20356b18b5ae95105d65566
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-10-26 20:58:56 -0400

    updating GameComponents with new helpful TextureAtlas constructors.

--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef49406760207aa178b2aa695c3a6004931106b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91c71d4cd7412ffd08f4cc31b49f4545624f108")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
