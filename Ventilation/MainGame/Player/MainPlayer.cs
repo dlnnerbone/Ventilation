14ba6600c615222e71019a8de309df385d9503bd
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-10-31 17:01:51 -0400

    removing Web class (temporary)

--- Ventilation/MainGame/Player/MainPlayer.cs
+++ Ventilation/MainGame/Player/MainPlayer.cs
@@ -9,12 +9,14 @@ namespace Main;
 public sealed class Player : Entity
 {
     public CharacterMovementModule Movement { get; private set; }
-    public CombatModule<WebClump> Combat { get; private set; }
     
     public Animation PlayerIdleAnimation { get; private set; }
     public TextureAtlas IdleAtlas { get; private set; }
     
-    public Player(int x, int y, int width = 196, int height = 196, float HP = 100) : base(x, y, width, height, HP) { }
+    public Player(int x, int y, int width = 196, int height = 196, float HP = 100) : base(x, y, width, height, HP)
+    {
+        
+    }
     public void LoadPlayerContent(ContentManager content) 
     {
         IdleAtlas = new TextureAtlas(4, 4, 256, 256);
