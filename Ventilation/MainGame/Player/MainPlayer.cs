8318b80eed9b641a4541045058cc93c02e66485e
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-11-28 16:44:40 -0500

    testing Particle System and drawing, stress testing.

--- Ventilation/MainGame/Player/MainPlayer.cs
+++ Ventilation/MainGame/Player/MainPlayer.cs
@@ -5,16 +5,23 @@ using Microsoft.Xna.Framework.Content;
 using GameComponents.Entity;
 using GameComponents.Rendering;
 using GameComponents;
+using System;
 namespace Main;
 public sealed class Player : Entity
 {
     public CharacterMovementModule Movement { get; private set; }
     public PlayerCombatModule combatModule { get; private set; }
+    public readonly ParticleManager particleManager;
     
     public Sprite PlayerSprite { get; private set; }
     public bool IsAlive { get; set; } = true;
+    private Texture2D texture;
     
-    public Player() : base(50, 250, 32 * 4, 32 * 4, 100, 0, 100) {}
+    public Player() : base(50, 250, 32 * 4, 32 * 4, 100, 0, 100) 
+    {
+        particleManager = new(100);
+        particleManager.Initialize(() => new Particle());
+    }
     
     public void LoadContent(GraphicsDevice device, ContentManager content) 
     {
@@ -23,6 +30,9 @@ public sealed class Player : Entity
         PlayerSprite.SetData(Color.White);
         
         combatModule = new(content);
+        
+        texture = new(device, 1, 1);
+        texture.SetData(new Color[] { Color.Red });
     }
     
     public void UpdatePlayer(GameTime gt) 
@@ -31,6 +41,17 @@ public sealed class Player : Entity
         MoveAndSlide(gt);
         Movement.UpdateMovement(gt, this);
         combatModule.UpdateCombat(gt, this);
+        int count = 0;
+        
+        particleManager.UpdateParticles((ref Particle p) => 
+        {
+            count++;
+            p.LayerDepth = 1f;
+            
+            p.Radians += (float)gt.ElapsedGameTime.TotalSeconds * (float)Math.Sqrt(count);
+            
+            p.Position = Center - new Vector2(5, 5) + p.Direction * new Vector2(300f / (float)Math.Sqrt(count), 100f / (float)Math.Sqrt(count));
+        });
     }
     
     public void DrawPlayer(SpriteBatch spriteBatch) 
@@ -38,5 +59,6 @@ public sealed class Player : Entity
         if (!IsAlive) return;
         PlayerSprite.Draw(spriteBatch, Bounds);
         combatModule.DrawCombat(spriteBatch);
+        particleManager.Draw(spriteBatch, texture, new Rectangle(0, 0, 10, 10), new Vector2(5, 5)); // 
     }
 }
--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de94086288aa95114870741e129402ab29e09fc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042a418c04432377183aef8fb3f16e510545d139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
