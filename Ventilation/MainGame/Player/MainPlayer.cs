622dac03a1d687b29cc33442012aa618dfb6f592
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-12-01 21:49:34 -0500

    removing particles as they aren't needed and testing json serialization with player data.

--- Ventilation/MainGame/Player/MainPlayer.cs
+++ Ventilation/MainGame/Player/MainPlayer.cs
@@ -1,13 +1,13 @@
 using Microsoft.Xna.Framework;
-using GameComponents.Managers;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using GameComponents.Entity;
 using GameComponents.Rendering;
 using GameComponents.Helpers;
-using System;
-using System.Text.Json.Serialization;
+using Microsoft.Xna.Framework.Input;
+using GameComponents.Managers;
 namespace Main;
+
 public sealed class Player : Entity
 {
     public CharacterMovementModule Movement { get; set; }
@@ -16,33 +16,15 @@ public sealed class Player : Entity
     public Sprite PlayerSprite { get; private set; }
     public bool IsAlive { get; set; } = true;
     
-    private Texture2D pTexture;
-    private ParticleManager pM;
-    
-    public Player() : base(50, 250, 32 * 4, 32 * 4, 100, 0, 100) 
-    {
-        pM = new ParticleManager(100);
-        pM.Initialize(() => new Particle 
-        {
-            LayerDepth = 1f,
-            LifeTime = 1f,
-            Age = 0f,
-            Position = Vector2.Zero
-        });
-        
-    }
+    public Player() : base(50, 250, 32 * 4, 32 * 4, 100, 0, 100) {}
     
     public void LoadContent(GraphicsDevice device, ContentManager content) 
     {
         Movement = new CharacterMovementModule(content);
         PlayerSprite = new Sprite(device, 1, 1);
         PlayerSprite.SetData(Color.White);
-        PlayerSprite.LayerDepth = 0.1f;
         
         combatModule = new(content);
-        
-        pTexture = new(device, 1, 1);
-        pTexture.SetData(new Color[] { Color.Red });
     }
     
     public void UpdatePlayer(GameTime gt) 
@@ -52,26 +34,6 @@ public sealed class Player : Entity
         Movement.UpdateMovement(gt, this);
         combatModule.UpdateCombat(gt, this);
         
-        var count = 0;
-        pM.UpdateParticles((ref Particle p, int i) => 
-        {
-            count++;
-            var sqrtCount = (float)Math.Sqrt(count);
-            if (i < 51) 
-            {
-                p.Radians += (float)gt.ElapsedGameTime.TotalSeconds * 2 * sqrtCount * 0.3f;
-                p.Radians %= (float)Math.PI * 2;
-                
-                p.Position = Center - new Vector2(5, 5) + p.Direction * 100f / sqrtCount * 2f;
-            }
-            else 
-            {
-                p.Radians -= (float)gt.ElapsedGameTime.TotalSeconds * sqrtCount * 0.85f;
-                p.Radians %= (float)Math.PI * 2;
-                Diagnostics.Write($"{p.Radians}");
-                p.Position = Center - new Vector2(5, 5) + p.Direction * 1000f / sqrtCount;
-            }
-        });
     }
     
     public void DrawPlayer(SpriteBatch spriteBatch) 
@@ -79,6 +41,5 @@ public sealed class Player : Entity
         if (!IsAlive) return;
         PlayerSprite.Draw(spriteBatch, Bounds);
         combatModule.DrawCombat(spriteBatch);
-        pM.Draw(spriteBatch, pTexture, new Rectangle(0, 0, 10, 10), new Vector2(5, 5));
     }
 }
--- Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
+++ Ventilation/obj/Debug/net8.0/Ventilation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d69644db699c4687590bc429c5cd64b83386c62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34f91e7c0c7c6c8591e47aecf58ec7d5d43dc51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ventilation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
